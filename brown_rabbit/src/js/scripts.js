ba4987875d4b28546b176056b75f3cce081cb6ce
Shweta <shweta.malav@gmail.com>
2023-10-24 21:51:27 +0200

    Searchbar is stylled. Articles are rendered. Cropped and resized images for corresponding articles are added. And database for artcles is created.

--- brown_rabbit/src/js/scripts.js
+++ brown_rabbit/src/js/scripts.js
@@ -1,5 +1,33 @@
+const coffeeContainer = document.querySelector('.posts');
 const logoList = document.querySelector('#logos');
 
+// Articles with images and description
+
+function loadCoffeeItems() {
+    coffeeItems.forEach((item) => {
+        const coffeeItem = document.createElement('a');
+       // coffeeItem.href = '#';
+        coffeeItem.className = 'item';
+
+        const itemContent = `
+            <div>
+                <img src="${item.imageSrc}" alt="${item.altText}">
+            </div>
+            <div id="item-desc">
+                <h2>${item.title}</h2>
+                <time datetime="2021-12-07">Published <span>12/07/2021</span></time>
+                <p>${item.desc}</p>
+                <button onclick="openPage(this)">Read more</button>
+            </div>
+        `;
+
+        coffeeItem.innerHTML = itemContent;
+        coffeeContainer.appendChild(coffeeItem);
+    });
+};
+
+
+// Sponsors logos 
 function loadSponsorImages() {
     logos.forEach((logo) => {
         const logoImage = document.createElement('img');
@@ -9,6 +37,8 @@ function loadSponsorImages() {
     });
 }
 
-window.addEventListener('load', function () {
-    loadSponsorImages();
-});
+
+        window.addEventListener('load', function () {
+            loadSponsorImages();
+            loadCoffeeItems()
+        });
