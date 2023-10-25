d3661f3541acdbae14737bf83e08af9331edde8d
Shweta <shweta.malav@gmail.com>
2023-10-25 11:51:58 +0200

    Popup Article is updated.

--- brown_rabbit/src/js/scripts.js
+++ brown_rabbit/src/js/scripts.js
@@ -23,8 +23,54 @@ const loadCoffeeItems = () => {
 
         coffeeItem.innerHTML = itemContent;
         coffeeContainer.appendChild(coffeeItem);
+
+
+
+        const popup = document.createElement('div');
+        popup.className = 'popup';
+
+        const popupContent = `
+            <div>
+                <img src="${item.imageSrc}" alt="${item.altText}">
+            </div>
+            
+            <div class="popUpDescription">
+                <h2>${item.title}</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque vitae tempus quam pellentesque. Donec ultrices tincidunt arcu non sodales neque sodales ut etiam. Lectus arcu bibendum at varius vel pharetra. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. Quam pellentesque nec nam aliquam sem et. Enim blandit volutpat maecenas volutpat blandit aliquam etiam. </p>
+                <p>Adipiscing at in tellus integer feugiat scelerisque. Augue interdum velit euismod in. Sed velit dignissim sodales ut eu sem integer vitae. Tempus imperdiet nulla malesuada pellentesque. Nulla pellentesque dignissim enim sit. Eget sit amet tellus cras adipiscing enim eu turpis egestas. Dictum at tempor commodo ullamcorper. Hac habitasse platea dictumst quisque. Egestas diam in arcu cursus euismod.</p>
+                <p>Donec enim diam vulputate ut. Ut aliquam purus sit amet. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Praesent tristique magna sit amet purus gravida quis. Ornare lectus sit amet est placerat in egestas. Maecenas pharetra convallis posuere morbi leo urna molestie. Tincidunt praesent semper feugiat nibh sed pulvinar. Id cursus metus aliquam eleifend mi.</p>
+                <p>Egestas diam in arcu cursus euismod quis viverra nibh. Gravida neque convallis a cras semper auctor neque vitae. Amet consectetur adipiscing elit ut aliquam purus. Sodales neque sodales ut etiam sit amet nisl purus in. Lorem sed risus ultricies tristique nulla aliquet enim tortor. Lectus arcu bibendum at varius vel pharetra.</p>
+                <button onclick="closePage(this)">Close</button>
+            </div>
+            `;
+        popup.innerHTML = popupContent;
+        coffeeContainer.appendChild(popup);
+
     });
+
+    const hrElement = document.createElement('hr');
+    coffeeContainer.appendChild(hrElement);
+
+    const buttonsDiv = document.createElement('div');
+    buttonsDiv.className = 'buttons';
+    const buttonsContent = `
+        <button>Previous</button>
+        <button>Next</button>
+    `;
+    buttonsDiv.innerHTML = buttonsContent;
+    coffeeContainer.appendChild(buttonsDiv);
 };
+
+function openPage(button) {
+    const popup = button.parentNode.parentNode.nextElementSibling;
+    popup.classList.remove('closePopUp');
+    popup.classList.add('openPopUp');
+}
+
+function closePage(button) {
+    const popup = button.parentNode.parentNode;
+    popup.classList.add('closePopUp');
+}
 
 
 //Search coffee articles
@@ -63,10 +109,11 @@ function loadSponsorImages() {
         logoImage.alt = logo.altText;
         logoList.appendChild(logoImage);
     });
-}
+};
 
+//Window onload function
 
-        window.addEventListener('load', function () {
-            loadSponsorImages();
-            loadCoffeeItems()
-        });
+window.addEventListener('load', function () {
+    loadSponsorImages();
+    loadCoffeeItems()
+});       
